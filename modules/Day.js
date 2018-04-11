2fd4d7cf1ebcf47f7984592fd3a65342ce3a86f4
Andrey Vokin <andrey.vokin@gmail.com>
2018-04-11 17:11:09 +0200

    LocalStorageDataProvider

--- modules/Day.js
+++ modules/Day.js
@@ -1,14 +1,51 @@
 import React from 'react'
 import Calendar from "./Calendar";
+import {INSTANCE} from "./LocalStorageDataProvider";
 
 export default class Day extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.addTask = this.addTask.bind(this);
+        this.deleteTask = this.deleteTask.bind(this);
+
+        this.dayString = this.getDayString();
+        let tasks = INSTANCE.getTasksFor(this.dayString);
+
+        this.state = {tasks: tasks}
+    }
+
     render() {
-        const dayString = this.getDayString();
+        const listItems = this.state.tasks.map((task) =>
+            <tr key={task.id}>
+                <td className="vertical-aligned text-center">
+                    {this.completeBlock(task)}
+                </td>
+                <td className="vertical-aligned">
+                    {task.title}
+                </td>
+                <td className="vertical-aligned">
+                    <div className="input-group vertical-aligned">
+                        <input type="text" className="form-control"
+                               placeholder="Due date"/>
+                        <div className="input-group-append">
+                            <button className="btn btn-outline-secondary"
+                                    type="button">
+                                <i className="fa fa-calendar"/>
+                            </button>
+                        </div>
+                    </div>
+                </td>
+                <td onClick={() => this.deleteTask(task)} className="text-right vertical-aligned">
+                    <button className="btn btn-secondary">Delete</button>
+                </td>
+            </tr>
+        );
 
         return <div className="container-fluid">
             <div className="row">
                 <div className="col col-md-4">
-                    <Calendar day={dayString}/>
+                    <Calendar day={this.dayString}/>
                 </div>
                 <div className="col col-md-8">
                     <div className="container-fluid">
@@ -16,38 +53,14 @@ export default class Day extends React.Component {
                             <div className="col col-md-12">
                                 <table className="table table-striped">
                                     <tbody>
-                                    <tr>
-                                        <td className="vertical-aligned text-center">
-                                            {/*<i className="fa fa-check"></i>*/}
-                                            <button className="btn btn-secondary">
-                                                Complete
-                                            </button>
-                                        </td>
-                                        <td className="vertical-aligned">
-                                            Task1
-                                        </td>
-                                        <td className="vertical-aligned">
-                                            <div className="input-group vertical-aligned">
-                                                <input type="text" className="form-control"
-                                                       placeholder="Due date"/>
-                                                <div className="input-group-append">
-                                                    <button className="btn btn-outline-secondary"
-                                                            type="button">
-                                                        <i className="fa fa-calendar"/>
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td className="text-right vertical-aligned">
-                                            <button className="btn btn-secondary">Delete</button>
-                                        </td>
-                                    </tr>
+                                    {listItems}
                                     <tr>
                                         <td colSpan="4">
                                             <div className="input-group">
-                                                <input className="form-control" placeholder="New task..."/>
+                                                <input className="form-control" placeholder="New task..."
+                                                       ref={input => this.newTaskTitle = input}/>
                                                 <span className="input-group-btn">
-                                                  <button className="btn btn-secondary">
+                                                  <button onClick={this.addTask} className="btn btn-secondary">
                                                     Add
                                                   </button>
                                                 </span>
@@ -64,13 +77,40 @@ export default class Day extends React.Component {
         </div>
     }
 
+    addTask() {
+        const title = this.newTaskTitle.value;
+
+        let task = {title: title, dueDay: this.dayString, isComplete: false};
+        INSTANCE.addTask(task);
+        this.setState({tasks: INSTANCE.getTasksFor(this.dayString)});
+    }
+
+    deleteTask(task) {
+        INSTANCE.removeTask(task);
+        this.setState({tasks: INSTANCE.getTasksFor(this.dayString)});
+    }
+
+    completeTask(task) {
+
+    }
+
+    completeBlock(task) {
+        if (task.isComplete) {
+            return <i className="fa fa-check"/>
+        }
+        return (
+            <button className="btn btn-secondary" onClick={this.completeTask(task)}>
+                Complete
+            </button>
+        )
+    }
+
     getDayString() {
         if (this.props.params.day != null) {
             return this.props.params.day;
         } else {
             const today = new Date();
-            console.log(Day.formatTwoDigits(today.getMonth()));
-            return today.getFullYear() + Day.formatTwoDigits(today.getMonth() + 1) + Day.formatTwoDigits(today.getDay() + 1);
+            return today.getFullYear() + Day.formatTwoDigits(today.getMonth() + 1) + Day.formatTwoDigits(today.getDate());
         }
     }
 
---
+++ modules/LocalStorageDataProvider.js
@@ -0,0 +1,57 @@
+class LocalStorageDataProvider {
+    getAllTasks() {
+        if (this.myTasks == null) {
+            let data = window.localStorage.getItem("tasks");
+            if (data == null) {
+                this.myTasks = [];
+                LocalStorageDataProvider.serializeTasks(this.myTasks);
+            } else {
+                this.myTasks = JSON.parse(data);
+            }
+        }
+        return this.myTasks;
+    }
+
+    removeTask(task) {
+        const tasks = this.getAllTasks();
+        let i = tasks.indexOf(task);
+        if (i >= 0) {
+            tasks.splice(i, 1)
+        }
+
+        LocalStorageDataProvider.serializeTasks(tasks);
+    }
+
+    addTask(task) {
+        const tasks = this.getAllTasks();
+        let maxId = 0;
+        for (let i = 0; i < tasks.length; i++) {
+            let t = tasks[i];
+            if (t.id > maxId) {
+                maxId = t.id;
+            }
+        }
+        task.id = maxId + 1;
+        tasks.push(task);
+
+        LocalStorageDataProvider.serializeTasks(tasks);
+    }
+
+    getTasksFor(day) {
+        let tasks = this.getAllTasks();
+        let result = [];
+        for (let i = 0; i < tasks.length; i++) {
+            const task = tasks[i];
+            if (task.dueDay < day  && !task.isComplete || task.dueDay === day) {
+                result.push(task)
+            }
+        }
+        return result;
+    }
+
+    static serializeTasks(tasks) {
+        window.localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+}
+
+export const INSTANCE = new LocalStorageDataProvider();
